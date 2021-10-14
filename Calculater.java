2e03557c8fa7d8c8fa45cca4a61a73b1a81945f9
shivakpriya <65661231+Shivapriya29@users.noreply.github.com>
2021-10-14 16:06:26 +0530

    creating  method and call the method in main class

--- Calculater.java
+++ Calculater.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 public static Calculater{
+	// main function
 	public static void main(String[] args){
+		// creating object to the class.
 	Calculater c = new Calculater();
 		Scanner s = new Scanner(System.in);
+		// print the value 
 		System.out.println("Enter a value");
 		int a = s.nextInt();
 		System.out.println("Enter b Value");
@@ -12,4 +15,21 @@ public static Calculater{
 		c.Multiplication(a,b);
 		c.Subtraction(a,b);
 	}
+	int Addition(int a, int b){
+		int p = a + b;
+		return p;
+	}
+	 int Division(int a , int b){
+		 int q = a / b;
+		 return q;
+	 }
+	int Multiplication(int a , int b){
+		int r = a * b;
+		return r ;
+	}
+	int Subtraction(int a , int b){
+		int t = a - b;
+		return t;
+	}
+	
 }
