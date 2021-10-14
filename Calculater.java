295aed0045a5c8c4582bca26007ae7278794270f
shivakpriya <65661231+Shivapriya29@users.noreply.github.com>
2021-10-14 15:28:16 +0530

    Calculater pro upadating main function

--- Calculater.java
+++ Calculater.java
@@ -1,4 +1,14 @@
 public static Calculater{
 	public static void main(String[] args){
+	Calculater c = new Calculater();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a value");
+		int a = s.nextInt();
+		System.out.println("Enter b Value");
+		int b = s.nextInt();
+		c.Addition(a,b);
+		c.Division(a,b);
+		c.Multiplication(a,b);
+		c.Subtraction(a,b);
 	}
 }
